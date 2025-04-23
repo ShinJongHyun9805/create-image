1c9bcd4efa106e2643bd047ded124d65bc629987
jhshin <jakie580@gmail.com>
2025-04-23 22:34:44 +0900

    최초 셋팅

---
+++ image/src/main/java/com/mcp/image/CreateImageApplication.java
@@ -0,0 +1,13 @@
+package com.mcp.image;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateImageApplication.class, args);
+	}
+
+}
---
+++ image/src/test/java/com/mcp/image/CreateImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcp.image;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
